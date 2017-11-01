8f9e38f617b17eb5d32cff69d4a02035cc4f005c
gabgob1066 <gabgob1066@gmail.com>
2017-11-01 11:42:04 +0000

    completed doomsday

--- Week5Template.py
+++ Week5Template.py
@@ -16,4 +16,22 @@
 # *** DO NOT CHANGE CODE ABOVE THIS LINE ***
 
 def doomsday(y):
-    
+    if y in range(1800,1900):
+    	x=5
+    elif y in range(1900,2000):
+    	x = 3
+    elif y in range(2000,2100):
+    	x = 2
+    elif y in range(2100,2200):
+    	x = 0
+    else:
+    	return -1
+
+    w = y%100
+    b = w%12
+    a = (w-b) // 12
+    c=b//4
+
+    d = (a+b+c)%7
+
+    return (d+x)%7
